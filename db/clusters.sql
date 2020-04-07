e9786248a08e7987da454dc0c582cb031bbbecfa
Gastón Caruso <gstn.caruso@gmail.com>
2020-04-07 02:22:18 -0300

    Refactor seeds

---
+++ db/clusters.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b438d6d18311a7e02c83a15c8d978f4344ff95b04487f14d91fb92f837b173b6
+size 323586
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,10 @@
 ActiveRecord::Base.logger = Logger.new(STDOUT)
 
-seeds = %W[#{Rails.root}/db/main_clusters.sql #{Rails.root}/db/main_conformer_pairs.sql #{Rails.root}/db/main_conformers.sql]
+seeds = %W[#{Rails.root}/db/clusters.sql #{Rails.root}/db/pairs.sql #{Rails.root}/db/conformers.sql]
 
 connection = ActiveRecord::Base.connection
 
 seeds.each do |seed|
-  sql = File.read(seed)
-  statements = sql.split(/;$/)
-  statements.pop
-
-  ActiveRecord::Base.transaction do
-    statements.each do |statement|
-      connection.execute(statement)
-    end
-  end
+  statements = File.read(seed)
+  ActiveRecord::Base.transaction { connection.execute(statements) }
 end
