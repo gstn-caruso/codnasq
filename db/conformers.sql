714ec2eb575b56ce3f94abcb1eea6ec47e8f0422
Gastón Caruso <gstn.caruso@gmail.com>
2020-04-07 02:22:18 -0300

    Refactor seeds

---
+++ db/conformers.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5303dfb0b1cf604367041a0bd459006232fe11026eacda72f8bfcd51d754aef2
+size 7353045
---
+++ pairs.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:001e2331de5664ea20b9886f3e5c4dbad9d3be2f0c44249b9375c76f2a7b129c
+size 113146472
