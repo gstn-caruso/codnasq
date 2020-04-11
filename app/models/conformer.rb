f6d61158597ee441d924d17f88f6383cd9445a21
Gastón Caruso <gstn.caruso@gmail.com>
2020-04-10 23:58:55 -0300

    Refactor Conformer class>>#search_in_all_fields

--- app/models/conformer.rb
+++ app/models/conformer.rb
@@ -2,8 +2,10 @@ class Conformer < ApplicationRecord
   validates_uniqueness_of :pdb_id
   belongs_to :cluster, primary_key: :codnasq_id
 
+  CONFORMER_FULL_SEARCH_COLUMNS = %w[pdb_id name organism ligands description uniprot_id gene_names pfam_id].freeze
+
   scope :search_in_all_fields, ->(text) do
-    where(column_names.map { |field| "#{field} like '%#{text}%'" }.join(' or '))
+    where(CONFORMER_FULL_SEARCH_COLUMNS.map { |field| "#{field} like '%#{text}%'" }.join(' or '))
   end
 
   def pfam_ids
