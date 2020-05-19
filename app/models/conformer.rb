5782f10c7512f37029b9e2d053cf0bc83fb76ce7
Gastón Caruso <gstn.caruso@gmail.com>
2020-05-19 03:29:07 -0300

    fixes #68

--- app/models/conformer.rb
+++ app/models/conformer.rb
@@ -5,7 +5,7 @@ class Conformer < ApplicationRecord
   CONFORMER_FULL_SEARCH_COLUMNS = %w[pdb_id name organism ligands description uniprot_id gene_names pfam_id].freeze
 
   scope :search_in_all_fields, ->(text) do
-    where(CONFORMER_FULL_SEARCH_COLUMNS.map { |field| "#{field} like '%#{text}%'" }.join(' or '))
+    where(CONFORMER_FULL_SEARCH_COLUMNS.map { |field| "lower(#{field}) like '%#{text}%'" }.join(' or '))
   end
 
   def pfam_ids
