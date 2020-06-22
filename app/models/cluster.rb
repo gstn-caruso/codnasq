57f1d0c2e2f8d5856ff17ae202005e332a0a7865
Nahuel Escobedo <nahuelescobedo@gmail.com>
2020-06-22 20:35:26 +0200

    renaming group

--- app/models/cluster.rb
+++ app/models/cluster.rb
@@ -2,6 +2,7 @@ class Cluster < ApplicationRecord
   has_one :conformer_pair
   validates_uniqueness_of :codnasq_id
 
+  alias_attribute(:group, :cluster_group)
   def conformers_amount
     conformers.count
   end
