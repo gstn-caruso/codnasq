ca00084c3cbd86e7113287cf6ba76a6deb58e38d
nahuel <nahuelescobedo@gmail.com>
2021-04-07 11:02:51 -0300

    Revert "Fixes #105"
    This reverts commit f92696a4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_011636) do
+ActiveRecord::Schema.define(version: 2020_06_27_235048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,9 +50,9 @@ ActiveRecord::Schema.define(version: 2020_07_01_011636) do
   end
 
   create_table "conformers", force: :cascade do |t|
-    t.string "cluster_id", null: false
-    t.string "pdb_id", null: false
-    t.integer "biological_assembly", null: false
+    t.string "cluster_id"
+    t.string "pdb_id"
+    t.integer "biological_assembly"
     t.float "resolution"
     t.string "method"
     t.integer "length"
