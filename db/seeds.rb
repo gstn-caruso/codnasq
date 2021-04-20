5770ab145fd0ef3c86faffbe7d56e153725d453d
Nahuel Escobedo <nahuelescobedo@gmail.com>
2021-04-20 10:53:30 -0300

    big csv arreglo de link 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 # csv_location = "#{Rails.root}/db/dbq.csv"
-csv_location = open("https://drive.google.com/uc?id=1YRcOCq8HS2lYFM0FRFUgmgEQFNiRyZMT")
+csv_location = open("https://drive.google.com/u/0/uc?export=download&confirm=BW3R&id=1YRcOCq8HS2lYFM0FRFUgmgEQFNiRyZMT")
 
 clusters = []
 conformers = []
